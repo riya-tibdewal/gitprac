fdec6635904d662cba90b73a16487e532797161a
Riya Tibdewal <riya.tibdewal@gmail.com>
2025-09-10 16:59:59 +0530

    comments added

---
+++ index.js
@@ -0,0 +1,3 @@
+//add the first 6 commands first so it adds the readme.md file first then uske baad we add the untracked file of our code so do git add .
+//git commit -m "comments"
+//git push -u origin main
