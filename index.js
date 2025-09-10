d7c2d738cea88fa98f6e8252e71e3c382525e55c
Riya Tibdewal <riya.tibdewal@gmail.com>
2025-09-10 17:32:09 +0530

    added changes

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 //add the first 6 commands first so it adds the readme.md file first then uske baad we add the untracked file of our code so do git add .
 //git commit -m "comments"
 //git push -u origin main
+//all the files i dont want to push on github i can add in gitignore file like node modules
+//git log is used to get the history of all the commits it gives 4 imp pieces realted to commits
+//git revert 
+//git reset --soft 
+//git reset --hard
+
+//git pull-to fetch all the files and add to our local respository 
